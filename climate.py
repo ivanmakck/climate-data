0e7c4880f94873f427c874638a39234105b1601b
Ivan Mak <ivanmakck@gmail.com>
2022-06-27 08:40:24 -0400

    Refactored python script

--- climate.py
+++ climate.py
@@ -1,26 +1,27 @@
 import pandas as pd
 import os
+import glob
 
-data_path = './data'
-if not os.path.exists(data_path):
-    os.mkdir(data_path)
+paths = {
+    'data_path': './data',
+    'output_path': './output'
+}
+
+for path in paths.values():
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+csvs = glob.glob(os.path.join(paths['data_path'], '*.csv'))
+out_filepath = os.path.join(paths['output_path'], 'all_years.csv')
 
 dfs = []
-for filename in os.listdir(data_path):
-    filepath = os.path.join(data_path, filename)
-    if '.csv' in filename:
-        if len(dfs) == 0:
-            df = pd.read_csv(filepath, index_col=None)
-            dfs.append(df)
-        else:
-            df = pd.read_csv(filepath, header=0, index_col=None)
-            dfs.append(df)
+for csv in csvs:
+    if len(dfs) == 0:
+        df = pd.read_csv(csv, index_col=None)
+        dfs.append(df)
+    else:
+        df = pd.read_csv(csv, header=0, index_col=None)
+        dfs.append(df)
 
 all_years = pd.concat(dfs, ignore_index=True)
-
-output_path = './output'
-if not os.path.exists(output_path):
-    os.mkdir(output_path)
-
-out_filepath = os.path.join(output_path, 'all_years.csv')
 all_years.to_csv(out_filepath, index=False)
